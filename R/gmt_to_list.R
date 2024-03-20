8ab74425e4314ecddf16d7167c70a47e593f1f47
TylerSagendorf <tjsagendorf@gmail.com>
2024-03-20 12:57:49 -0500

    Add gmt_to_list function
    Creates a named list of gene sets from a GMT file. Added unit tests, as well.

---
+++ R/gmt_to_list.R
@@ -0,0 +1,35 @@
+#' @title Convert GMT file to named list
+#'
+#' @description
+#' Create a named list of sets from a GMT file.
+#'
+#' @param path character; path to a GMT file.
+#'
+#' @returns A named list of character vectors.
+#'
+#' @export gmt_to_list
+#'
+#' @note Equivalent to \code{fgsea::gmtPathways}.
+#'
+#' @examples
+#' path <- system.file("extdata", "c5.go.v2023.2.Hs.symbols.gmt",
+#'                     package = "ostRich")
+#'
+#' x <- gmt_to_list(path)
+#'
+#' head(names(x)) # First 6 gene set names
+#'
+#' x[1] # first set
+
+gmt_to_list <- function(path) {
+  if (!grepl("\\.gmt(\\..+)?$", path))
+    stop("`path` is not a path to a GMT file.")
+
+  gmt <- readLines(path)
+  gmt <- strsplit(gmt, split = "\t")
+
+  out <- lapply(gmt, function(x) x[-c(1L, 2L)])
+  names(out) <- vapply(gmt, function(x) x[1L], character(1L))
+
+  return(out)
+}
---
+++ tests/testthat/test-gmt_to_list.R
@@ -0,0 +1,41 @@
+test_that("non-GMT files throw an error", {
+  expect_error(
+    gmt_to_list("non_gmt_file.txt"),
+    "`path` is not a path to a GMT file."
+  )
+
+  expect_error(
+    gmt_to_list("non_gmt_file.gmtt"),
+    "`path` is not a path to a GMT file."
+  )
+})
+
+
+test_that("GMT files can be compressed", {
+  expect_error(
+    suppressWarnings(
+      gmt_to_list("fake_file.gmt.gzip")
+    ),
+    "cannot open the connection" # fake file does not exist
+  )
+})
+
+
+test_that("gmt_to_list output is correct", {
+  path <- system.file("extdata", "c5.go.v2023.2.Hs.symbols.gmt",
+                      package = "ostRich")
+
+  x <- gmt_to_list(path)
+
+  # Correct number of sets
+  expect_equal(length(x), 10461L)
+
+  # Sets are named
+  expect_true(!is.null(names(x)))
+
+  # Spot check first set
+  expect_identical(
+    x[["GOBP_10_FORMYLTETRAHYDROFOLATE_METABOLIC_PROCESS"]],
+    c("AASDHPPT", "ALDH1L1", "ALDH1L2", "MTHFD1", "MTHFD1L", "MTHFD2L")
+  )
+})
