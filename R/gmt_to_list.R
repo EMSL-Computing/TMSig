1e441280071d7abbd625006fdf8b601a3df2e07a
TylerSagendorf <tjsagendorf@gmail.com>
2024-03-30 11:08:57 -0500

    Update gmt_to_list
    Add check argument to allow non-GMT files that have the correct format.

--- R/gmt_to_list.R
+++ R/gmt_to_list.R
@@ -4,12 +4,13 @@
 #' Create a named list of sets from a GMT file.
 #'
 #' @param path character; path to a GMT file.
+#' @param check logical; check that \code{path} points to a valid GMT file.
 #'
 #' @returns A named list of character vectors.
 #'
 #' @export gmt_to_list
 #'
-#' @note Equivalent to \code{fgsea::gmtPathways}.
+#' @note Similar to \code{fgsea::gmtPathways}.
 #'
 #' @examples
 #' path <- system.file("extdata", "c5.go.v2023.2.Hs.symbols.gmt.gz",
@@ -21,15 +22,15 @@
 #'
 #' x[1] # first set
 
-gmt_to_list <- function(path) {
-  if (!grepl("\\.gmt(\\..+)?$", path))
+gmt_to_list <- function(path, check = TRUE) {
+  if (check & !grepl("\\.gmt(\\..+)?$", path))
     stop("`path` is not a path to a GMT file.")
 
   gmt <- readLines(path)
   gmt <- strsplit(gmt, split = "\t")
 
-  out <- lapply(gmt, function(x) x[-c(1L, 2L)])
-  names(out) <- vapply(gmt, function(x) x[1L], character(1L))
+  out <- lapply(gmt, function(x) x[-c(1, 2)])
+  names(out) <- vapply(gmt, function(x) x[1], character(1L))
 
   return(out)
 }
