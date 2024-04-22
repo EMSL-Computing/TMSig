f26cf96a75c53d04eb2c5042c7c489c1fbf43984
TylerSagendorf <tjsagendorf@gmail.com>
2024-04-22 18:38:31 -0500

    Update gmt_to_list
    Update documentation, stricter pattern matching.

--- R/gmt_to_list.R
+++ R/gmt_to_list.R
@@ -1,10 +1,12 @@
-#' @title Convert GMT file to named list
+#' @title Convert GMT file to named list of sets
 #'
-#' @description
-#' Create a named list of sets from a GMT file.
+#' @description Create a named list of sets from a GMT file, or a file
+#'   structured like a GMT.
 #'
 #' @param path character; path to a GMT file.
-#' @param check logical; check that \code{path} points to a valid GMT file.
+#' @param check logical; check that \code{path} points to a valid GMT file. If
+#'   \code{FALSE}, files with different extensions may be read, so long as they
+#'   are in the expected format.
 #'
 #' @returns A named list of character vectors.
 #'
@@ -23,14 +25,17 @@
 #' x[1] # first set
 
 gmt_to_list <- function(path, check = TRUE) {
-  if (check & !grepl("\\.gmt(\\..+)?$", path))
+  if (check & !grepl("\\.gmt(\\.[^\\.]+)?$", path))
     stop("`path` is not a path to a GMT file.")
 
   gmt <- readLines(path)
   gmt <- strsplit(gmt, split = "\t")
 
+  # This assumes that the first entry of each line is the set name and the
+  # elements begin at the third entry.
   out <- lapply(gmt, function(x) x[-c(1, 2)])
-  names(out) <- vapply(gmt, function(x) x[1], character(1L))
+  names(out) <- vapply(gmt, FUN = function(x) x[1],
+                       FUN.VALUE = character(1L))
 
   return(out)
 }
