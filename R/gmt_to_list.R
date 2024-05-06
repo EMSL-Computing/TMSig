ae9e56eee10a406db5631ab58734e2eb0719e6cb
TylerSagendorf <tjsagendorf@gmail.com>
2024-05-06 11:24:42 -0500

    Rename package most --> TMSig
    MOST is taken on CRAN. Renamed to TMSig.

--- R/gmt_to_list.R
+++ R/gmt_to_list.R
@@ -17,7 +17,7 @@
 #'
 #' @examples
 #' path <- system.file("extdata", "c5.go.v2023.2.Hs.symbols.gmt.gz",
-#'                     package = "most")
+#'                     package = "TMSig")
 #'
 #' x <- gmt_to_list(path)
 #'
